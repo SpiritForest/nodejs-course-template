e50027a146ea54656af2a6047aaffb34673adac7
SpiritForest <dzakhar@adnoc.ae>
2021-05-28 21:24:35 +0300

    docs: add jsdoc for board

--- src/resources/boards/board.memory.repository.js
+++ src/resources/boards/board.memory.repository.js
@@ -8,19 +8,56 @@
 //   }]
 // }];
 
+/**
+ * Board memory module
+ * @module board_repository
+ */
+
 const BOARDS = [];
 
+/**
+ * Returs all the boards
+ * @returns {Promise} Promise representation of array of boards
+ */
 const getAll = async () => BOARDS;
 
+/**
+ * Adds board to the DB
+ * @param {object} board Board object
+ * @param {string} board.id String representation of board Id
+ * @param {String} board.title String representation of board title
+ * @param {object[]} board.columns Array of board columns
+ * @param {string} board.columns.id String representation of board column id
+ * @param {string} board.columns.title String representation of board column title
+ * @param {number} board.columns.order Board column order
+ * @returns {Promise} Promise object represents board data
+ */
 const addData = async (board) => {
   BOARDS.push(board);
   return board;
 };
 
+/**
+ * Replaces board data with a new one
+ * @param {number} index Index of board in DB
+ * @param {object} board Board object
+ * @param {string} board.id String representation of board Id
+ * @param {String} board.title String representation of board title
+ * @param {object[]} board.columns Array of board columns
+ * @param {string} board.columns.id String representation of board column id
+ * @param {string} board.columns.title String representation of board column title
+ * @param {number} board.columns.order Board column order
+ * @returns {Promise} Promise representation of result of replacing data
+ */
 const replaceData = async (index, data) => {
   BOARDS.splice(index, 1, data);
 };
 
+/**
+ * Deletes board by specified index
+ * @param {number} index 
+ * @returns {Promise} Promise object represents result of data deletion
+ */
 const deleteByIndex = async (index) => {
   BOARDS.splice(index, 1);
 };
--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,20 +1,62 @@
+/**
+ * Board Service module
+ * @module board_service
+ */
+
 const repo = require('./board.memory.repository');
 const taskService = require('../tasks/task.memory.repository');
 
+/**
+ * Geta all the boards from DB
+ * @returns {Promise} Promise representaion of result getting boards
+ */
 const getAll = async () => repo.getAll();
 
+/**
+ * Returns board index in DB
+ * @param {string} id String representation of board id
+ * @returns {Promise} Promise object represents board index in DB
+ */
 const getDataIndexById = async (id) => {
   const boards = await getAll();
   return boards.findIndex(data => data.id === id);
 };
 
+/**
+ * Returns board data by specified id
+ * @param {string} id String representation of board id
+ * @returns {Promise} Promise object represents board data
+ */
 const getById = async (id) => {
   const boards = await getAll();
   return boards.find(data => data.id === id);
 };
 
+/**
+ * Adds board data to the DB
+ * @param {object} data Board object
+ * @param {string} data.id String representation of board Id
+ * @param {String} data.title String representation of board title
+ * @param {object[]} data.columns Array of board columns
+ * @param {string} data.columns.id String representation of board column id
+ * @param {string} data.columns.title String representation of board column title
+ * @param {number} data.columns.order Board column order
+ * @returns {Promise} Promise object represents board data
+ */
 const addData = async (data) => repo.addData(data);
 
+/**
+ * Replaces data for specifies board with a new one
+ * @param {string} id String representation of board id
+ * @param {object} data Board object
+ * @param {string} data.id String representation of board Id
+ * @param {String} data.title String representation of board title
+ * @param {object[]} data.columns Array of board columns
+ * @param {string} data.columns.id String representation of board column id
+ * @param {string} data.columns.title String representation of board column title
+ * @param {number} data.columns.order Board column order
+ * @returns {Promise} Promise object represents board data
+ */
 const putData = async (id, data) => {
   const index = await getDataIndexById(id);
   
@@ -25,6 +67,11 @@ const putData = async (id, data) => {
   return data;
 };
 
+/**
+ * Deletes baord data from DB
+ * @param {string} id String representation of board id
+ * @returns {Promise} Promise object represents board index in DB
+ */
 const deleteData = async (id) => {
   const index = await getDataIndexById(id);
 
