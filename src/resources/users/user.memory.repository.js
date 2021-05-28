a2dee83330655f4a727077de43170079914c6c5e
SpiritForest <dzakhar@adnoc.ae>
2021-05-28 21:25:16 +0300

    docs: add jsdoc for users

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,3 +1,8 @@
+/**
+ * User Memory module
+ * @module user_repository
+ */
+
 // const USERS = [{
 //   id: "052b7462-458c-4a08-a6ca-d9ddae6735e1",
 //   name: "Vasya",
@@ -12,19 +17,44 @@
 
 const USERS = [];
 
+/**
+ * Returns all users
+ * @returns {Promise} Promise object represents array of users
+ */
 const getAll = async () => USERS;
 
+/**
+ * Adds a user to the DB
+ * @param {{id: string, name: string, login: string, password: string}} user Object with properties which represents user data
+ * @returns {Promise} Promise object represents user data
+ */
 const addData = (user) => {
   USERS.push(user);
   return user;
 };
 
+/**
+ * Returns a user by specified id
+ * @param {string} id String representation of user id
+ * @returns {Promise} Promise object represents user data
+ */
 const getById = (id) => USERS.find(user => user.id === id);
 
+/**
+ * Replaced user data with a new one
+ * @param {number} index Index of user in DB array
+ * @param {{id: string, name: string, login: string, password: string}} data Data which will replace the existing one
+ * @returns {Promise} Promise object represents user data was replaced
+ */
 const replaceData = async (index, data) => {
   USERS.splice(index, 1, data);
 };
 
+/**
+ * Deletes user from DB by index
+ * @param {index} index Index of user in which will be deleted
+ * @returns {Promise} Promise object represents that user was deleted
+ */
 const deleteByIndex = async (index) => {
   USERS.splice(index, 1);
 };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,20 +1,50 @@
+/**
+ * User Service module
+ * @module user_service
+ */
+
 const repo = require('./user.memory.repository');
 const taskRepo = require('../tasks/task.memory.repository');
 
+/**
+ * Returns all users
+ * @returns {Promise} Promise object represents array of users
+ */
 const getAll = async () => repo.getAll();
 
+/**
+ * Returns index user data in DB by specified id
+ * @param {string} id String representation of user id
+ * @returns {Promise} Promise object represents index data in DB
+ */
 const getDataIndexById = async (id) => {
   const users = await getAll()
   return users.findIndex(data => data.id === id);
 };
 
+/**
+ * Returns user data by specified id
+ * @param {string} id String representation of user id
+ * @returns {Promise} Promise object represents user data
+ */
 const getById = async (id) => {
   const users = await getAll();
   return users.find(data => data.id === id);
 };
 
+/**
+ * Adds user data to DB
+ * @param {{id: string, name: string, login: string, password: string}} data user data
+ * @returns {Promise} Promise object represents that user data was added
+ */
 const addData = (data) => repo.addData(data);
 
+/**
+ * Replaces user data by user id
+ * @param {string} id String representation of user id
+ * @param {{id: string, name: string, login: string, password: string}} data user data
+ * @returns {Promise} Promise object represets user data
+ */
 const putData = async (id, data) => {
   const index = await getDataIndexById(id);
   
@@ -25,6 +55,11 @@ const putData = async (id, data) => {
   return data;
 };
 
+/**
+ * Deletes user by specified id
+ * @param {string} id String representation of user id
+ * @returns {Promise} Promise object represents index of data
+ */
 const deleteData = async (id) => {
   const index = await getDataIndexById(id);
 
